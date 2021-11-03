a5afc3df70b8d3ebb0668e4a2ffe3f5f46ae4354
Kiwifan1 <70110252+Kiwifan1@users.noreply.github.com>
2021-11-03 11:31:40 -0700

    more creation notes

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,8 @@
  * Description: A pet store using Linked lists to store data
  * Notes: I will likely be writing everything from scratch again, to avoid the issue I held in the previous PAs with counting
  * 
- * 
+ *  11/3/21 - Added variables
+ *  
  **/
 
 #include "header.h"
--- definitions.cpp
+++ definitions.cpp
@@ -6,7 +6,7 @@
  * Description: A pet store using Linked lists to store data
  * Notes: I will likely be writing everything from scratch again, to avoid the issue I held in the previous PAs with counting
  * 
- * 
+ *  11/3/21 - created definitions.cpp
  **/
 
 #include "header.h"
