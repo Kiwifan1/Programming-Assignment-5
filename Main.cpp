dd4d1c743d399b90903dabb443cec642cabe4acf
Kiwifan1 <70110252+Kiwifan1@users.noreply.github.com>
2021-11-03 11:10:59 -0700

    Programming Assignment 5 Declaration

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Name: Joshua Venable
+ * Class: CPSC 122, Fall 2021
+ * Date: 11/3/2021
+ * Programming Assigment: PA5
+ * Description: A pet store using Linked lists to store data
+ * Notes: I will likely be writing everything from scratch again, to avoid the issue I held in the previous PAs with counting
+ * 
+ * 
+ **/
+
+#include "header.h"
+
+int main()
+{
+    cout << "hello world";
+}
---
+++ header.h
@@ -0,0 +1,22 @@
+/**
+ * Name: Joshua Venable
+ * Class: CPSC 122, Fall 2021
+ * Date: 11/3/2021
+ * Programming Assigment: PA5
+ * Description: A pet store using Linked lists to store data
+ * Notes: I will likely be writing everything from scratch again, to avoid the issue I held in the previous PAs with counting
+ * 
+ * 
+ **/
+
+#ifndef HEADER_H
+#define HEADER_H
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#endif
